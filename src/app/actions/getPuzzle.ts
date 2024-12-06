9867101b2a54b706bf102e5717a36900187fdf14
Wonkyu <wonq33@gmail.com>
2024-12-06 14:49:29 -0500

    1.9.13 - Added a console log in getPuzzle

--- src/app/actions/getPuzzle.ts
+++ src/app/actions/getPuzzle.ts
@@ -5,6 +5,7 @@ import type { ClientPuzzle } from "~/server/types/puzzle";
 import { format } from "date-fns";
 
 export async function getPuzzle(date: string): Promise<ClientPuzzle>{
+  console.log("USERS DATE ON GETPUZZLE:", date);
   const data = await api.puzzle.get({ usersDate: format(date, "MM-dd-yyyy") });
 
   return data!;
