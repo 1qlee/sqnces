26ab6d41d9652ed202443c9fdbe42bd434ac4091
Wonkyu <wonq33@gmail.com>
2024-12-07 00:40:43 -0500

    1.9.17 - Hotfix: Rolling back that hack I inserted into game that was resetting for NEUTRONAL

--- src/app/components/game/Game.tsx
+++ src/app/components/game/Game.tsx
@@ -69,11 +69,6 @@ export default function Game() {
       }
     }
 
-    // temporary fix for puzzle change - remove after 12/6/24
-    if (!gameState.puzzle.words.find(word => word.sequence.string === "VAR")) {
-      void fetchPuzzle();
-    }
-
     // refresh stale localStorage puzzle data
     if (gameState.puzzle.date !== todaysDate) {
       void fetchPuzzle();
